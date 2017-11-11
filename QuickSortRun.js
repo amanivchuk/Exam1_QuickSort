332368a5e81b9abc003bc5555ee5861d772b4688
Alexandr Manivchuk <amanivchuk@gmail.com>
2017-11-11 20:12:27 +0200

    QuickSort run

--- QuickSortRun.js
+++ QuickSortRun.js
@@ -1,32 +1,29 @@
-
-var array = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
-
 function quicksort(array, left, right) {
     left = left || 0;
     right = right || array.length - 1;
 
     var tmp = sort(array, left, right);
 
-    if(left < tmp - 1) {
+    if (left < tmp - 1) {
         quicksort(array, left, tmp - 1);
     }
-    if(right > tmp) {
+    if (right > tmp) {
         quicksort(array, tmp, right);
     }
     return array;
 }
 
 function sort(array, left, right) {
-    var tmp = Math.floor((left + right) / 2 );
+    var tmp = Math.floor((left + right) / 2);
 
-    while(left <= right) {
-        while(array[left] < array[tmp]) {
+    while (left <= right) {
+        while (array[left] < array[tmp]) {
             left++;
         }
-        while(array[right] > array[tmp]) {
+        while (array[right] > array[tmp]) {
             right--;
         }
-        if(left <= right) {
+        if (left <= right) {
             swap(array, left, right);
             left++;
             right--;
@@ -41,5 +38,31 @@ function swap(array, i, j) {
     array[j] = temp;
 }
 
+function start() {
+    var array = [];
+    var tmp;
+    while (tmp = prompt("Enter numbers or end", "")) {
+        if (tmp === "end") {
+            console.log("Before sort");
+            printArray(array);
+            console.log("-------------");
+            console.log("After sort");
+            var sortArray = quicksort(array);
+            printArray(sortArray);
+            break;
+        }
+        if (Number(tmp)) {
+            array.push(tmp);
+        } else {
+            prompt("Wrong number! Enter new number!");
+        }
+    }
+}
 
-console.log(quicksort(array));
+function printArray(array) {
+    for (var i = 0; i < array.length; i++) {
+        console.log(array[i] + " ");
+    }
+}
+
+start();
